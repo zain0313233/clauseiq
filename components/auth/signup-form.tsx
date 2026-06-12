d9023b3f3123013c928a7c8dd73cac84ee11a14b
zain0313233 <zain0313233@gmail.com>
2026-06-13 00:59:10 +0500

    deploy to railway

--- components/auth/signup-form.tsx
+++ components/auth/signup-form.tsx
@@ -8,6 +8,9 @@ import { toast } from "sonner"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+
+
+
 import {
   Card,
   CardContent,
